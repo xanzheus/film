aa55429e8220562310e8d02605e3c0151b8dc682
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-07-24 21:56:18 +0300

    Prescribes comments for functions

--- src/js/request.service.js
+++ src/js/request.service.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const API__KEY = 'api_key=16092738eabd8acc3b7b5db91d1d6d26';
 
+// You can find examples of how functions work in the file "test-mosalov.js"
 export default class RequestService {
   constructor() {
     this.IMG__URL = 'https://image.tmdb.org/t/p/w500';
@@ -12,7 +13,7 @@ export default class RequestService {
     this.geners = 'genre/movie/list';
     this.searchQuery = '';
   }
-
+// This function await callback to output an error in hendler
   async getTrendingMovies(callback) {
     const url = `${this.trendingMovies}?${API__KEY}`;
     try {
@@ -23,7 +24,7 @@ export default class RequestService {
       callback();
     }
   }
-
+// This function await callback to output an error in hendler
   async getSearchMovies(callback) {
     const searchParams = new URLSearchParams({
       query: this.searchQuery,
@@ -38,7 +39,7 @@ export default class RequestService {
       callback();
     }
   }
-
+// This function await callback to output an error in hendler
   async getDescriptionMovie(movieId, callback) {
     const url = `${this.movieById}/${movieId}?${API__KEY}`;
     try {
