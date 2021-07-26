ec2f2c1ae84cad95b28feb378f90b1ef3c0aca0e
Nataliia Valko <75252511+NataliiaValko@users.noreply.github.com>
2021-07-26 05:43:21 +0300

    adds load cards. need to fixed genres

--- src/js/test-mosalov.js
+++ src/js/test-mosalov.js
@@ -1,17 +1,17 @@
-import RequestService from './request.service';
-// to get started, you need to create an instance of the class
-const requestServise = new RequestService();
+// import RequestService from './request.service';
+// // to get started, you need to create an instance of the class
+// const requestServise = new RequestService();
 
-// searchQuery this is the value of the input
-const searchQuery = 'Titanic';
+// // searchQuery this is the value of the input
+// const searchQuery = 'Titanic';
 
-// searchQuery this is the value of the input
-requestServise.query = searchQuery;
+// // searchQuery this is the value of the input
+// requestServise.query = searchQuery;
 
-requestServise.getTrendingMovies().then(console.log);
-requestServise.getSearchMovies().then(console.log);
-// function await "ID"
-requestServise.getDescriptionMovie(334455).then(console.log);
-requestServise.getGenresMovies().then(console.log);
-// To get the full url of the picture. Runing the first part
-console.log(requestServise.getPrefixUrlImg('/wob7xUAJS85zf6OtQaHZQUvVWjX.jpg'));
+// // requestServise.getTrendingMovies().then(console.log);
+// // requestServise.getSearchMovies().then(console.log);
+// // // function await "ID"
+// requestServise.getDescriptionMovie(334455).then(console.log);
+// // requestServise.getGenresMovies().then(console.log);
+// // To get the full url of the picture. Runing the first part
+// // console.log(requestServise.getPrefixUrlImg('/wob7xUAJS85zf6OtQaHZQUvVWjX.jpg'));
