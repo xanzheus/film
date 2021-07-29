e2a7b9f6a209390be449d4983b0f1abacebb3e75
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-07-29 15:25:33 +0300

    fixed js for tranding's card

--- src/js/result.js
+++ src/js/result.js
@@ -1,58 +1,65 @@
 import refs from './refs';
 import RequestService from './request.service';
-import markupCardTpl from '../templates/cardFilmTpl.hbs';
+import markupCardTpl from '../templates/cardFilmTrandingTpl.hbs';
+
 
 const requestService = new RequestService();
-const IMG__URL = 'https://image.tmdb.org/t/p/w500';
 
-const makeMarkupCardsList = (array) => {
-    console.log(array)
-    console.log(refs.resultAnchor)
-    refs.resultAnchor.insertAdjacentHTML('beforeend', markupCardTpl(array))
+const setResults = (response) => {
+ return response?.results
 }
 
-const makeValidatesPosterPath = url => {
-    return `${IMG__URL}${url}`;
+const makeMarkupCardsList = (array) => {    
+    refs.resultAnchor.insertAdjacentHTML('beforeend', markupCardTpl(array))
 }
 
 const makeValidatesReleaseDate = data => {
     return data.slice(0, 4);
 }
 
-const makeValidatesGenreName = array => {
-    array.forEach(object => {
-        // console.log(array)
-        // object.genre_names = [];
-        object.genre_ids.forEach(
-            (idGenre, indexGenre, arrayMain) => {
-                requestService.getGenresMovies()
-                    .then(array => {
-                        array.forEach(objectNames => {
-                            if (objectNames.id === idGenre) {
-                                object.genre_ids.splice(indexGenre, 1, objectNames['name'])
-                                
-                                // object.genre_names.push(objectNames['name'])
-                                // console.log(object.genre_names)
-                            }
-                           
-                        });
-                        
-                    })
-                // console.log(array)
-                // console.log(array)
-                
-            })
-    })
-    makeMarkupCardsList(array)
+const makefilterObject = ({poster_path, genre_ids,id, original_title, release_date, vote_average}) => {
+    const newObject = {};
+    newObject.poster_path = poster_path;
+    newObject.genre_ids = genre_ids;
+    newObject.id = id;
+    newObject.original_title = original_title;
+    newObject.release_date = release_date;
+    newObject.vote_average = vote_average;
+return newObject
 }
 
-// const setValidatesPosterPath = (array) => {
-//     array.forEach(object => { object.poster_path = makeValidatesPosterPath(object.poster_path) })
-//     return array
-// }
+const makefilterObjects = array => {
+    const shortArray = array.map(makefilterObject)
+    return shortArray
+}
+
+let genresList
+
+const setGenresList = (array) => {
+    genresList = [...array]
+}
+
+const makeValidatesGenreName = array => {
+    array.forEach(object => {
+        object.genre_ids.forEach(
+            (idGenre, indexGenre) => {
+                genresList.forEach(objectNames => {
+                    if (objectNames.id === idGenre) {
+                        object.genre_ids.splice(indexGenre, 1, objectNames['name'])
+                    }                           
+                })                        
+            })
+        })
+    return array
+    }
+
+const makeGenresList = () => {
+    requestService.getGenresMovies()
+        .then(setGenresList)
+}
 
 const setValidatesPosterPath = (array) => {
-    array.forEach(object => { object.poster_path = makeValidatesPosterPath(object.poster_path) })
+    array.forEach(object => { object.poster_path = requestService.getPrefixUrlImg(object.poster_path) })
     return array
 }
 
@@ -61,21 +68,22 @@ const setValidatesReleaseDate = (array) => {
     return array
 }
 
-const renderingTrendingCardsList = () => {
-    // const requestService = new RequestService();
-    requestService.getTrendingMovies()
-        .then(setValidatesPosterPath)
-        .then(setValidatesReleaseDate)
-        .then(makeValidatesGenreName)
-        // .then(array => { console.log(array[0].genre_names); return array })
-    // .then(console.log)
-        // .then(makeMarkupCardsList);
-}
-
 const clearCardsList = () => {
     refs.resultAnchor.innerHTML = '';
 }
 
+const renderingTrendingCardsList = () => {
+    clearCardsList()
+    requestService.getTrendingMovies()
+        .then(setResults)
+        .then(makefilterObjects)
+        .then(setValidatesPosterPath)
+        .then(setValidatesReleaseDate)
+        .then(makeValidatesGenreName)
+        .then(makeMarkupCardsList)
+}
+
+makeGenresList();
 renderingTrendingCardsList()
 
 export { clearCardsList, renderingTrendingCardsList}
