63cec89d19176dabe5cbb3f30cba698000b8da7a
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-07-29 18:53:07 +0300

    fixed modal details film

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import './sass/main.scss';
 
-import refs from './js/refs';
-import modal from './js/modal-window'
-import filmTpl from './templates/modal-window'
-
+import './js/modal-details-film';
 import './js/result';
 import './js/_back_to_top';
-
 import './js/local-storage';
 import './js/test-mosalov';
---
+++ src/js/modal-details-film.js
@@ -0,0 +1,59 @@
+import * as basicLightbox from 'basiclightbox'
+import 'basiclightbox/src/styles/main.scss'
+import refs from './refs';
+import requestService from './request.service'
+import {makeMarkup} from './modal-details-film-tpl'
+
+const request = new requestService
+
+const getMoreDetailfilm = function ({title, poster_path, vote_average, vote_count, popularity, original_title, overview,genres}) {
+    const descriptionFilm = {}
+    descriptionFilm.title = title;
+    descriptionFilm.poster_path = poster_path;
+    descriptionFilm.vote_average= vote_average;
+    descriptionFilm.vote_count = vote_count;
+    descriptionFilm.popularity = popularity;
+    descriptionFilm.original_title = original_title;
+    descriptionFilm.overview = overview;
+    descriptionFilm.genres = genres.reduce((acc, genre) => {return `${acc}${genre.name}, `}, '').slice(0, -2);
+    return descriptionFilm
+}
+
+const setValidatesBackdrop_path = (obj) => {
+    obj.poster_path = request.getPrefixUrlImg(obj.poster_path)
+      return obj
+}
+
+const doActionsShowModal = function (markup) {
+    showModal(markup);
+    refs.modalDetailsFilm = document.querySelector('.modal')
+    refs.modalDetailsFilmButtonClose = refs.modalDetailsFilm.querySelector('.modal .btn__close')
+    refs.modalDetailsFilmButtonClose.addEventListener('click',closeModalDetails)
+}
+
+const getActiveInfo = function (id) {
+    return request.getDescriptionMovie(id)
+        .then(getMoreDetailfilm)
+        .then(setValidatesBackdrop_path)
+        .then(makeMarkup)
+        .then(doActionsShowModal)        
+}
+ 
+let target = ''
+
+const showModal = function (markup) {
+   target = basicLightbox.create(markup);
+    target.show()
+}
+ 
+const closeModalDetails = function ()  {
+    target.close()
+     refs.modalDetailsFilmButtonClose.removeEventListener('click',closeModalDetails)
+}
+
+const renderModal = function (e) {
+     const id =e.target.dataset.id
+    getActiveInfo(id)
+}
+
+refs.resultAnchor.addEventListener('click', renderModal);
