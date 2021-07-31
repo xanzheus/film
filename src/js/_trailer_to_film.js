0eebfb059b8cde2d9cbf802ea8265bfe66cb5e05
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2021-07-31 11:35:36 +0200

    add 404 video if not correct trailer

--- src/js/_trailer_to_film.js
+++ src/js/_trailer_to_film.js
@@ -1,15 +1,23 @@
 import * as basicLightbox from 'basiclightbox';
+const axios = require('axios');
 import { API__KEY } from './request.service';
 
 function showTrailerToFilm(id) {
   const url = `https://api.themoviedb.org/3/movie/${id}/videos?${API__KEY}&language=en-US`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const id = data.results[0].key;
+  axios
+    .get(url)
+    .then(response => {
+      const id = response?.data?.results[0].key;
+      console.log(id);
       const instance = basicLightbox.create(`
   <iframe width="560" height="315" src='https://www.youtube.com/embed/${id}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 `);
+      instance.show();
+    })
+    .catch(() => {
+      const instance = basicLightbox.create(`
+        <iframe width="560" height="315" src='https://www.youtube.com/embed/TSXXi2kvl_0'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      `);
       instance.show();
     });
 }
@@ -19,7 +27,6 @@ export class ShowTrailer {
     this.id = id;
   }
   show() {
-    console.log(this.id);
     const btn = document.querySelector('[data-anchor="trailer"]');
     btn.addEventListener('click', () => {
       showTrailerToFilm(this.id);
