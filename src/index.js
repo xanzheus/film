b4dcd23a34fb4fb1cc187c22e5ce8996d6430f06
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2021-08-01 07:14:44 +0200

    add animation

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import './js/result';
 import './js/_back_to_top';
 import './js/local-storage';
 import './js/_trailer_to_film';
-import './js/header_switch';
+// import './js/header_switch';
 import './js/_magicMouse';
