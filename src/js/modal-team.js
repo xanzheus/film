54b2b1e2ff7dd2cf5d486557b304bdec892d77f8
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-08-03 19:16:07 +0300

    adds styles for modal team

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,18 +1,20 @@
 import team from './team-members.json';
 import itemsTemplate from '../templates/cardsOurTeam.hbs';
 import refs from './refs';
+import { addClassToElement, removeClassFromElement } from './actions-functions';
 
 refs.ftBtnEl.addEventListener('click', onCloseModal);
 refs.ftLinkEl.addEventListener('click', onOpenModal);
 
 function onCloseModal(event) {
-  console.log('click');
+  // console.log('click');
   refs.ftBackdropEl.classList.add('is-hidden');
 }
 
 function onOpenModal(event) {
   event.preventDefault();
-  refs.ftBackdropEl.classList.remove('is-hidden');
+  removeClassFromElement(refs.ftBackdropEl,'is-hidden');
+
 }
 
 const markUp = itemsTemplate(team);
