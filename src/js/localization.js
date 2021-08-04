ca73c7f642347e6a50a615f252b125224dfe14b4
MariievHennadii <marieff1990@gmail.com>
2021-08-04 16:09:58 +0300

    добавил локалСтор для локализации + фикс стилей

--- src/js/localization.js
+++ src/js/localization.js
@@ -2,39 +2,92 @@ import refs from './refs';
 
 const { langSwitch, searchInput, htmlDoc } = refs;
 
-const RU = '[lang=ru]';
-const EN = '[lang=en]';
+const RU = 'ru';
+const EN = 'en';
 
 const PLACEHOLDER_TEXT_EN = 'Search films';
 const PLACEHOLDER_TEXT_RU = 'Поиск фильмов';
+const LANGUAGE_ATTRIBUTE = 'language';
 
 const nodeSwitchDisplayProperty = (node, property) => {
     node.style.display = property;
 }
 
-const languageSwitchingOnCheckbox = () => {
-    let hide = EN;
-    let show = RU;
-
-    if(!langSwitch.checked) {
-        [hide, show] = [show, hide];
-    }
-
-    if(show === RU) {
-        searchInput.placeholder = PLACEHOLDER_TEXT_RU;
-        htmlDoc.setAttribute('lang', 'ru');
+const checkAndChangeLangAttributeOfHTML = (language) => {
+    if(language) {
+        htmlDoc.setAttribute('lang', language);
+        htmlDoc.style.display = 'unset';
+        return;
     } else {
-        searchInput.placeholder = PLACEHOLDER_TEXT_EN;
-        htmlDoc.setAttribute('lang', 'en');
+        htmlDoc.setAttribute('lang', EN);
     }
+}
 
-    document.querySelectorAll(hide).forEach((node) => {
-        nodeSwitchDisplayProperty(node, 'none');
+const changeSearchPlaceholderLanguage = (languageShow) => {
+    if(languageShow === EN) {
+        searchInput.placeholder = PLACEHOLDER_TEXT_EN;
+    } else if (languageShow === RU){
+        searchInput.placeholder = PLACEHOLDER_TEXT_RU;
+    }
+}
+
+const changeDocumentElementsLanguage = (languageShow, languageHide) => {
+    document.querySelectorAll(`[lang=${languageHide}]`).forEach((node) => {
+        if(node.tagName !== 'HTML') {
+            nodeSwitchDisplayProperty(node, 'none');
+        }
     });
 
-    document.querySelectorAll(show).forEach((node) => {
-        nodeSwitchDisplayProperty(node, 'unset');
+    document.querySelectorAll(`[lang=${languageShow}]`).forEach((node) => {
+        if(node.tagName !== 'HTML') {
+            nodeSwitchDisplayProperty(node, 'unset');
+        }    
     });
 }
+
+const changePageLanguage = (languageShow, languageHide) => {
+    checkAndChangeLangAttributeOfHTML(languageShow);
+    changeSearchPlaceholderLanguage(languageShow);
+    changeDocumentElementsLanguage(languageShow, languageHide);
+}
+
+const changePageLanguageAndSwitchLangCheckBox = (languageShow, languageHide) => {
+    changePageLanguage(languageShow, languageHide);
+
+    languageShow === RU
+         ? langSwitch.checked = true
+         : langSwitch.checked = false;
+}
+
+const languageSwitchingOnCheckbox = () => {
+    let languageHide = EN;
+    let languageShow = RU;
+
+    if(!langSwitch.checked) {
+        [languageHide, languageShow] = [languageShow, languageHide];
+    }
+
+    changePageLanguage(languageShow, languageHide);
+    localStorage.setItem(LANGUAGE_ATTRIBUTE, languageShow);
+}
+
+const checkLanguageInLocalStorageOnPageLoad = () => {
+    const language = localStorage.getItem(LANGUAGE_ATTRIBUTE);
+ 
+    checkAndChangeLangAttributeOfHTML(language);
+
+    if(language === null) {
+        localStorage.setItem(LANGUAGE_ATTRIBUTE, EN);
+        language = EN;
+    }
+    
+    if (language === EN) {
+        changePageLanguageAndSwitchLangCheckBox(EN, RU);
+    } else if (language === RU) {
+        changePageLanguageAndSwitchLangCheckBox(RU, EN);
+    }
+}
+
+checkLanguageInLocalStorageOnPageLoad();
 
 langSwitch.addEventListener('change', languageSwitchingOnCheckbox);
