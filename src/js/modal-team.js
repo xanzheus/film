7eb92e6adb676c6fa5713d7a78ce3ecb0203d3fa
VitaliyVovk76 <vovkvitol@gmail.com>
2021-08-06 15:51:53 +0300

    footer

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -3,18 +3,31 @@ import itemsTemplate from '../templates/cardsOurTeam.hbs';
 import refs from './refs';
 import { addClassToElement, removeClassFromElement } from './actions-functions';
 
-refs.ftBtnEl.addEventListener('click', onCloseModal);
 refs.ftLinkEl.addEventListener('click', onOpenModal);
+refs.ftBtnEl.addEventListener('click', onCloseModalBtn);
+refs.ftBackdropEl.addEventListener('click', onCloseModalBackdrop);
+window.addEventListener('keydown', onCloseModalEsc);
 
-function onCloseModal(event) {
-  // console.log('click');
+function onCloseModalBtn(event) {
   refs.ftBackdropEl.classList.add('is-hidden');
+}
+
+function onCloseModalEsc(event) {
+  if (event.code === 'Escape') {
+    console.log('Escape');
+    refs.ftBackdropEl.classList.add('is-hidden');
+  }
+}
+
+function onCloseModalBackdrop(event) {
+  if (event.target === event.currentTarget) {
+    refs.ftBackdropEl.classList.add('is-hidden');
+  }
 }
 
 function onOpenModal(event) {
   event.preventDefault();
-  removeClassFromElement(refs.ftBackdropEl,'is-hidden');
-
+  removeClassFromElement(refs.ftBackdropEl, 'is-hidden');
 }
 
 const markUp = itemsTemplate(team);
